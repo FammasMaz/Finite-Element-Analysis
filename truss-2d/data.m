e530c204fdb895293cb51eb51f3b1f6c0bce627a
Moeeze Hassa <fammas.maz@gmail.com>
2020-11-19 13:29:29 +0500

    data: Edit with intial Setup Problem
    reference: https://www.yumpu.com/en/document/read/34895198/3d-truss-tutorial-written
    
    kthanksbye

--- truss-2d/data.m
+++ truss-2d/data.m
@@ -2,72 +2,42 @@
 
 
 % The coordinates of nodes
-% [x, y] positions, row represents node number
-nXY = [
-  0 0
-  6 0
-  3 3
-  0 3
-  6 3
-  0 6
-  6 6
-  3 6
-  3 9
-  0 9
-  6 9
-  3 12
-  0 12
-  6 12
-  3 15
+% [x, y, z] positions, row represents node number
+nXYZ = [
+  0 60 0 % z free
+  0 0 24
+  0 0 -24
+  96 30 0
 ];
 
-% [node_i, node_j, material_prop], row represents truss element number
+% [node_i, node_j,  material_prop], row represents truss element number
 eProp = [
-1 2 1
-1 3 1
-2 3 1
-2 5 1
-1 4 1
-3 4 1
-3 5 1
-5 8 1
-4 8 1
-5 7 1
-6 4 1
-6 8 1
-8 7 1
-6 9 1
-7 9 1
-6 10 1
-7 11 1
-9 10 1
-9 11 1
-10 12 1
-11 12 1
-10 13 1
-11 14 1
-12 13 1
-12 14 1
-14 15 1
-13 15 1
+  1 4 1
+  1 2 0
+  1 3 0
+  2 3 0
+  2 4 0
+  3 4 0
 ];
 
 
 % Boundary conditions on any given trusses
 % [node_id, x_axis_condition, y_axis_condition] where 1 = fixed; 0 = free
 bc = [
-1 1 1
-2 0 1
+1 1 1 1
+2 1 1 1
+3 1 1 1
 ];
 
 % Material Properties and Area of the Cross_sec area of the elements
 % [mod_of_elasticity, cross_area], where each row is a different material
 mater = [
-200e9 6.25e-3
+29e6 0.5
+11e6 1
 ];
 
 % Specifies any loads on any nodes
 % [node_id, Fx, Fy]
 loads = [
-15 500000 0
+4 0 -1500 0
 ];
