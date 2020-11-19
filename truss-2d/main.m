24baf0a330ce42bdd5896a948be6b7c6d984e2aa
Moeeze Hassa <fammas.maz@gmail.com>
2020-11-19 14:21:48 +0500

    update: use 3-d trusses

--- truss-2d/main.m
+++ truss-2d/main.m
@@ -1,4 +1,4 @@
-function [uxy, strain, stress, axialload] = main(data)
+function [uxy, strain, stress,k, K, axialload] = main(data)
 % Description:-
 %  - Solves the plane truss problems
 %  - Uses the data provided in data.m
@@ -16,11 +16,11 @@ run(data);
 % Caclucates necessary parameters
 nElems = size(eProp,1);
 nNodes = size(nXY,1);
-motion_axes = 2; % Axes where element can deform in
+motion_axes = 3; % Axes where element can deform in
 DOF = motion_axes * nNodes; % Degree of Freedom of Body
 
 % Initialization of required empty matrices
-k = zeros(2); % Stiffness matrix of Element
+k = zeros(3); % Stiffness matrix of Element
 K = zeros(DOF); % Stiffness matrix of System
 u = F = zeros(DOF,1); % Global Displacement and Force vectors
 axialload = stress = strain = l = zeros(nElems,1); % Output Matrices
@@ -35,16 +35,21 @@ for e=1:nElems
   % Coordinates of the first node
   xi = nXY(i, 1);
   yi = nXY(i, 2);
+  zi = nXY(i, 3);
+
   % Coordinates of the second node
   xj = nXY(j, 1);
   yj = nXY(j, 2);
+  zj = nXY(j, 3);
+
 
   % Length of the vector, i.e. the length of the element e
-  l(e) = sqrt((xj-xi)^2 + (yj-yi)^2);
+  l(e) = sqrt((xj-xi)^2 + (yj-yi)^2 + (zj -zi)^2);
 
   % Necessary Rotation Matrix element calculations
   c = (xj - xi)/l(e);
   s = (yj - yi)/l(e);
+  v = (zj - zi)/l(e);
 
   % Defining Material props for each element
   E = mater(m,1);
@@ -52,24 +57,26 @@ for e=1:nElems
 
   % Stiffness Matrix for each element
   k = A*E/l(e) * [
-  c*c s*c
-  c*s s*s
+  c*c c*s c*v
+  s*c s*s s*v
+  v*c v*s v*v
   ];
 
   % Assembly Matrix by putting k into K
-  n = 2*i - 1;
-  p = 2*j - 1;
-  K(n:n+1, n:n+1) = K(n:n+1, n:n+1) + k;
-  K(n:n+1, p:p+1) = K(n:n+1, p:p+1) - k;
-  K(p:p+1, n:n+1) = K(p:p+1, n:n+1) - k;
-  K(p:p+1, p:p+1) = K(p:p+1, p:p+1) + k;
+  n = 3*i - 2;
+  p = 3*j - 2;
+  K(n:n+2, n:n+2) = K(n:n+2, n:n+2) + k;
+  K(n:n+2, p:p+2) = K(n:n+2, p:p+2) - k;
+  K(p:p+2, n:n+2) = K(p:p+2, n:n+2) - k;
+  K(p:p+2, p:p+2) = K(p:p+2, p:p+2) + k;
 endfor
 
 % Loads in Global Coordinate system
 
 for n = 1:size(loads, 1)
-  F(2*loads(n,1)-1) = loads(n, 2); % X-Coordinate
-    F(2*loads(n,1)) = loads(n, 3); % Y-Coordinate
+  F(3*loads(n,1)-2) = loads(n, 2); % X-Coordinate
+  F(3*loads(n,1)-1) = loads(n, 3); % Y-Coordinate
+  F(3*loads(n,1)) = loads(n, 4)
 endfor
 
 % finding row to thin-down matrix, considering boundary conditions
@@ -79,7 +86,7 @@ bcrem = zeros(DOF, 1);
 
 for n = 1:size(bc, 1)
   bcnode = bc(n,1);
-  bcrem(2*bc(n,1)-1:2*bc(n,1)) = bc(n,2:3);
+  bcrem(3*bc(n,1)-2:3*bc(n,1)) = bc(n,2:4);
 
 endfor
 
@@ -111,19 +118,22 @@ for e=1:nElems
   % Coordinates of the first node
   xi = nXY(i, 1);
   yi = nXY(i, 2);
+  zi = nXY(i, 3);
   % Coordinates of the second node
   xj = nXY(j, 1);
   yj = nXY(j, 2);
+  zj = nXY(j, 3);
 
   % Length of the vector, i.e. the length of the element e
-  l(e) = sqrt((xj-xi)^2 + (yj-yi)^2);
+  l(e) = sqrt((xj-xi)^2 + (yj-yi)^2 + (zj - zi)^2);
 
   % Necessary Rotation Matrix element calculations
   c = (xj - xi)/l(e);
   s = (yj - yi)/l(e);
+  v = (yj - yi)/l(e);
 
   % Strain, Stress and Axial Load calculations from theoretical formulae
-  strain(e) = (((c * u(j*2-1) + s * u(j*2))) - ((c * u(i*2-1) + s * u(i*2)))) / l(e);
+  strain(e) = ((c * u(j*3-2)) + (s * u(j*3-1)) + (v*u(j*3))) - ((c * u(i*3-2)) + (s * u(i*3-1)) + (v*u(i*3))) / l(e);
   stress(e) = mater(m, 1) * strain(e);
   axialload(e) = mater(m, 2) * stress(e);
 
@@ -131,9 +141,10 @@ endfor
 
 % Recreating the strain matrix containing x and y displacements in a single row
 
-for i=1:DOF/2
-   uxy(i,1) = u(2*i-1);
-   uxy(i,2) = u(2*i);
+for i=1:DOF/3
+   uxy(i,1) = u(3*i-2);
+   uxy(i,2) = u(3*i-1);
+   uxy(i,3) = u(3*i)
 endfor
 
 endfunction
